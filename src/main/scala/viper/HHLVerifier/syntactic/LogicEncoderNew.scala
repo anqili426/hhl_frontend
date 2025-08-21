96334dc66c7dcadcaf8a4973dc246a6a3560e76e
David Hagen <dhagen@student.ethz.ch>
2025-08-21 12:00:27 +0200

    LogicEncoder: Encoding all environments for states and integer variables on the fly instead of pre-populating

--- src/main/scala/viper/HHLVerifier/syntactic/LogicEncoderNew.scala
+++ src/main/scala/viper/HHLVerifier/syntactic/LogicEncoderNew.scala
@@ -28,17 +28,13 @@ class LogicEncoderNew {
    * Cf. HHL paper, definition 3. */
   private val S: ArrayExpr[ArraySort[IntSort, IntSort], BoolSort] = ctx.mkConst("S", SetSort).asInstanceOf[ArrayExpr[ArraySort[IntSort, IntSort], BoolSort]]
 
-  /** Environment that maps program variable names to their Z3 integer constants.
+  /** Environment that maps program variable names and logical variable names to their Z3 integer constants.
    * Populated on the fly when encountering program variables in hyper-assertions. */
   private val progEnv: mutable.Map[String, IntExpr] = mutable.Map.empty[String, IntExpr]
 
   /** Environment that maps state variable names (introduced by quantifiers) to Z3
-   * array constants of sort [[StateSort]]. Populated by [[generateQuantifiedVars]]. */
+   * array constants of sort [[StateSort]]. Populated on the fly when encountering state variables in hyper-assertions. */
   private val stateEnv: mutable.Map[String, ArrayExpr[IntSort, IntSort]] = mutable.Map.empty[String, ArrayExpr[IntSort, IntSort]]
-
-  /** Environment that maps quantified variable names (other than states, currently integers)
-   * to their Z3 integer constants. Populated by [[generateQuantifiedVars]]. */
-  private val quantifiedEnv: mutable.Map[String, IntExpr] = mutable.Map.empty[String, IntExpr]
 
   /**
    * Checks whether the precondition `pre` ''logically entails'' the weakest
@@ -47,8 +43,6 @@ class LogicEncoderNew {
    *
    * @param pre The user-supplied precondition.
    * @param wp The weakest precondition computed by [[WeakestPrecondition.compute]]
-   * @param progVars All program variables that might occur in `pre` or `wp`. This are
-   *                 usually all parameters passed to the method
    * @return Z3 [[Status]]:
    *         <ul>
    *          <li>`UNSATISFIABLE` – <code>pre ⊨ wp</code> is valid.</li>
@@ -57,11 +51,7 @@ class LogicEncoderNew {
    *          <li>`UNKNOWN` – solver aborted.</li>
    *         </ul>
    */
-  def checkEntailment(pre: viper.HHLVerifier.ast.Expr, wp: viper.HHLVerifier.ast.Expr, progVars: Seq[Id]): (Status, Option[Model]) = {
-    // generate all necessary state variables in Z3 and add them to the environment
-    generateQuantifiedVars(pre)
-    generateQuantifiedVars(wp)
-
+  def checkEntailment(pre: viper.HHLVerifier.ast.Expr, wp: viper.HHLVerifier.ast.Expr): (Status, Option[Model]) = {
     // encode the precondition and WP
     val z3Pre = encodeBool(WeakestPrecondition.desugarQuantifiers(pre))
     val z3WP = encodeBool(WeakestPrecondition.desugarQuantifiers(wp))
@@ -94,7 +84,7 @@ class LogicEncoderNew {
     case UnaryExpr("!", e) => ctx.mkNot(encodeBool(e))
     case ImpliesExpr(left, right) => ctx.mkImplies(encodeBool(left), encodeBool(right))
     case Assertion(quantifier, List(AssertVarDecl(vName, StateType())), body) => {
-      val constantsArray: Array[com.microsoft.z3.Expr[_]] = Array(stateEnv(vName.name))
+      val constantsArray: Array[com.microsoft.z3.Expr[_]] = Array(getStateEnv(vName.name))
       val encodedBody: com.microsoft.z3.BoolExpr = encodeBool(body)
       quantifier match {
         case "exists" => ctx.mkExists(constantsArray, encodedBody, 0, null, null, null, null)
@@ -102,7 +92,7 @@ class LogicEncoderNew {
       }
     }
     case Assertion(quantifier, List(AssertVarDecl(vName, _)), body) => { // quantifier over non-state variable
-      val constantsArray: Array[com.microsoft.z3.Expr[_]] = Array(quantifiedEnv(vName.name))
+      val constantsArray: Array[com.microsoft.z3.Expr[_]] = Array(getProgEnv(vName.name))
       val encodedBody: com.microsoft.z3.BoolExpr = encodeBool(body)
       quantifier match {
         case "exists" => ctx.mkExists(constantsArray, encodedBody, 0, null, null, null, null)
@@ -111,15 +101,16 @@ class LogicEncoderNew {
     }
     case LookupExpr(AssertVar(stateName), Id(varName)) => sys.error("LogicEncoder: Unexpected LookupExpr in boolean conversion: " + expr.toString)
     case LookupExpr(id, index) => encodeBool(resolveLookup(index)(id.asInstanceOf[AssertVar]))
-    case StateExistsExpr(AssertVar(name), _) => ctx.mkSelect(S, stateEnv(name)).asInstanceOf[BoolExpr]
+    case StateExistsExpr(AssertVar(name), _) => ctx.mkSelect(S, getStateEnv(name)).asInstanceOf[BoolExpr]
     case _ => sys.error("LogicEncoder: Unexpected expression in boolean conversion: " + expr.toString)
   }
 
   private def encodeInt(expr: viper.HHLVerifier.ast.Expr): IntExpr = expr match {
-    case LookupExpr(AssertVar(stateName), Id(varName)) => ctx.mkSelect(stateEnv(stateName), progEnv.getOrElseUpdate(varName, ctx.mkIntConst(varName))).asInstanceOf[IntExpr]
+    case LookupExpr(AssertVar(stateName), Id(varName)) => ctx.mkSelect(getStateEnv(stateName), getProgEnv(varName)).asInstanceOf[IntExpr]
     case LookupExpr(id, index) => encodeInt(resolveLookup(index)(id.asInstanceOf[AssertVar]))
+    case Id(name) => getProgEnv(name) // a program variable shouldn't occur outside of a LookupExpr. However, there can be free variables from the exists rule
     case Num(value) => ctx.mkInt(value)
-    case AssertVar(name) => quantifiedEnv(name)
+    case AssertVar(name) => getProgEnv(name)
     case BinaryExpr(e1, op, e2) => op match {
       case "+" => ctx.mkAdd(encodeInt(e1), encodeInt(e2)).asInstanceOf[IntExpr]
       case "-" => ctx.mkSub(encodeInt(e1), encodeInt(e2)).asInstanceOf[IntExpr]
@@ -131,34 +122,12 @@ class LogicEncoderNew {
     case _ => sys.error("LogicEncoder: Unexpected expression in integer conversion: " + expr.toString)
   }
 
-  private def generateQuantifiedVars(expr: viper.HHLVerifier.ast.Expr): Unit = expr match {
-    case BinaryExpr(e1, _, e2) => {
-      generateQuantifiedVars(e1)
-      generateQuantifiedVars(e2)
-    }
-    case UnaryExpr(_, e) => generateQuantifiedVars(e)
-    case ImpliesExpr(left, right) => {
-      generateQuantifiedVars(left)
-      generateQuantifiedVars(right)
-    }
-    case Assertion(_, assertVarDecls, body) => {
-      assertVarDecls.foreach {
-        case AssertVarDecl(AssertVar(name), StateType()) => {
-          if (!stateEnv.contains(name)) {
-            val z3Var = ctx.mkConst(name, StateSort).asInstanceOf[ArrayExpr[IntSort, IntSort]]
-            stateEnv += (name -> z3Var)
-          }
-        }
-        case AssertVarDecl(AssertVar(name), IntType()) => {
-          if (!quantifiedEnv.contains(name)) {
-            val z3Var = ctx.mkIntConst(name)
-            quantifiedEnv += (name -> z3Var)
-          }
-        }
-      }
-      generateQuantifiedVars(body)
-    }
-    case _ => ()
+  private def getProgEnv(s: String): com.microsoft.z3.IntExpr = {
+    progEnv.getOrElseUpdate(s, ctx.mkIntConst(s))
+  }
+
+  private def getStateEnv(s: String): com.microsoft.z3.ArrayExpr[IntSort, IntSort] = {
+    stateEnv.getOrElseUpdate(s, ctx.mkConst(s, StateSort).asInstanceOf[ArrayExpr[IntSort, IntSort]])
   }
 
   private def resolveLookup(expr: viper.HHLVerifier.ast.Expr)(implicit assertVar: AssertVar): viper.HHLVerifier.ast.Expr = expr match {
