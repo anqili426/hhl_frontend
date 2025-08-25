6882187542d7d7ce18f996614de9c673690ba8fa
David Hagen <dhagen@student.ethz.ch>
2025-08-25 12:24:53 +0200

    Extend automatic test suite. Now tests all deterministic examples from the benchmark, which don't require framing, i.e. all supported features at this moment

--- src/test/scala/TestRunnerEval.scala
+++ src/test/scala/TestRunnerEval.scala
@@ -1,35 +1,26 @@
+import org.scalatest.concurrent.TimeLimits.cancelAfter
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.concurrent.TimeLimits.{cancelAfter, failAfter}
 import org.scalatest.time.SpanSugar._
 import viper.HHLVerifier.generation.Generator
 import viper.HHLVerifier.symbols.SymbolChecker
 import viper.HHLVerifier.typing.TypeChecker
 
-import scala.jdk.CollectionConverters._
 import java.nio.file.{Files, Path, Paths}
+import scala.jdk.CollectionConverters._
 
-class TestRunner extends AnyFunSuite {
+class TestRunnerEval extends AnyFunSuite {
 
   // Location of test programs
-  private val programDir: Path = Paths.get(getClass.getResource("/hypra-tests").toURI)
+  private val programDir: Path = Paths.get(getClass.getResource("/eval-tests").toURI)
 
   // Arguments we want to run the verifier with on the test programs
   private val args = Array("--auto", "--syntactic")
-
-  // Regex for checking "// VALID: <bool>" annotation at beginning of .hhl file
-  private val ValidTag = """(?i)//\s*valid\s*:\s*(true|false)\s*""".r
 
   Files.list(programDir).iterator().asScala
     .filter(Files.isRegularFile(_)) // skip directories
     .filter(_.getFileName.toString.toLowerCase.endsWith(".hhl")) // only .hhl files
     .foreach { file =>
-      val lines = Files.readAllLines(file).asScala
-      val expected = lines.headOption.flatMap {
-        case ValidTag(flag) => Some(flag.toBoolean)
-        case _ => None
-      }.getOrElse(
-        throw new IllegalArgumentException(s"$file is missing the // VALID: <bool> header")
-      )
+      val expected = !file.getFileName.toString.endsWith("false.hhl")
 
       test(file.getFileName.toString) {
         cancelAfter(15.seconds) {
