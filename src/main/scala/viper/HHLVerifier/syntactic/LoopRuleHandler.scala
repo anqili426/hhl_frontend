fb5d812167b1cf73313d372e8e5b5b96c59fd99e
David Hagen <dhagen@student.ethz.ch>
2025-08-28 09:43:45 +0200

    Semantic fix in `forallExists` rule

--- src/main/scala/viper/HHLVerifier/syntactic/LoopRuleHandler.scala
+++ src/main/scala/viper/HHLVerifier/syntactic/LoopRuleHandler.scala
@@ -71,7 +71,7 @@ object ForallExistsHandler extends LoopRuleHandler {
   private def computeLoopPost(inv: viper.HHLVerifier.ast.Expr, noForallAfterExists: Boolean = true)(implicit loopCondition: viper.HHLVerifier.ast.Expr): viper.HHLVerifier.ast.Expr = inv match {
     case Assertion("exists", List(AssertVarDecl(vName, vType)), BinaryExpr(StateExistsExpr(_, false), "&&", realBody)) =>
       Assertion("exists", List(AssertVarDecl(vName, vType)),
-        BinaryExpr(computeLoopPost(realBody, false), "&&", ImpliesExpr(UnaryExpr("!", loopCondition), StateExistsExpr(vName, false)))
+        BinaryExpr(computeLoopPost(realBody, false), "&&", ImpliesExpr(LookupExpr(vName, UnaryExpr("!", loopCondition)), StateExistsExpr(vName, false)))
       ) // cf. Hypra paper, p. 19, bottom
     case Assertion("exists", _, _) => sys.error("ForallExistsHandler: Tried to apply \"forallExistsRule\", but found non-desugared quantifier.")
     case Assertion("forall", assertVarDecls@List(AssertVarDecl(_, vType)), body) =>
