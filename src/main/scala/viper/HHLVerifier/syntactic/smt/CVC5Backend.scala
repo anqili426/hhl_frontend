940b01c393eb2bfdd334692eb027fb7ea6042e42
David Hagen <dhagen@student.ethz.ch>
2025-10-09 12:52:17 +0200

    Improving CVC5 encoding such that shadowed variables and free states (e.g., existsRule) are handled properly.

--- src/main/scala/viper/HHLVerifier/syntactic/smt/CVC5Backend.scala
+++ src/main/scala/viper/HHLVerifier/syntactic/smt/CVC5Backend.scala
@@ -21,19 +21,33 @@ class CVC5Backend extends SMTBackend {
   private val S_err = tm.mkConst(SFunSort, "S_err")
 
   private val constEnv: mutable.Map[String, Term] = mutable.Map.empty[String, Term]
-  private val varEnv: mutable.Map[String, Term] = mutable.Map.empty[String, Term]
+  private val stateEnv: mutable.Map[String, Term] = mutable.Map.empty[String, Term] // for free states resulting from some loop rules
+
+  private case class VarEnv(stateVars: Map[String, Term], intVars: Map[String, Term]) {
+    def withState(name: String): (VarEnv, Term) = {
+      val newVar = tm.mkVar(StateSort, name)
+      (copy(stateVars = stateVars + (name -> newVar)), newVar)
+    }
+    def withInt(name: String): (VarEnv, Term) = {
+      val newVar = tm.mkVar(IntSort, name)
+      (copy(intVars = intVars + (name -> newVar)), newVar)
+    }
+    def getState(name: String): Term = stateVars.getOrElse(name, stateEnv.getOrElseUpdate(name, tm.mkConst(StateSort, name))) // some loop rules have free states, which need to be treated as constants
+    def getInt(name: String): Term = intVars.getOrElse(name, sys.error(s"CVC5Backend: Unbound integer variable '$name'"))
+  }
+  private object VarEnv {
+    def empty: VarEnv = VarEnv(Map.empty, Map.empty)
+  }
 
   private def getConstEnv(s: String): Term = {
+    //println("const: " + s)
+    //System.out.flush()
     constEnv.getOrElseUpdate(s, tm.mkConst(IntSort, s))
   }
 
-  private def getVarEnv(s: String, state: Boolean): Term = {
-    varEnv.getOrElseUpdate(s, tm.mkVar(if(state) StateSort else IntSort, s))
-  }
-
   def checkEntailment(pre: viper.HHLVerifier.ast.Expr, wp: viper.HHLVerifier.ast.Expr): SMTStatus = {
-    val cvc5Pre = encodeBool(WeakestPrecondition.desugarQuantifiers(pre))
-    val cvc5WP = encodeBool(WeakestPrecondition.desugarQuantifiers(wp))
+    val cvc5Pre = encodeBool(WeakestPrecondition.desugarQuantifiers(pre))(VarEnv.empty)
+    val cvc5WP = encodeBool(WeakestPrecondition.desugarQuantifiers(wp))(VarEnv.empty)
     val cvc5FinalFormula = tm.mkTerm(Kind.NOT, tm.mkTerm(Kind.IMPLIES, cvc5Pre, cvc5WP))
 
     // solve ¬(pre ⇒ wp)
@@ -49,7 +63,7 @@ class CVC5Backend extends SMTBackend {
     else SMTStatus.Unknown
   }
 
-  private def encodeBool(expr: Expr): Term = expr match {
+  private def encodeBool(expr: Expr)(implicit env: VarEnv): Term = expr match {
     case BoolLit(value) => if (value) tm.mkTrue() else tm.mkFalse()
     case BinaryExpr(e1, op, e2) => op match {
       case "&&" => tm.mkTerm(Kind.AND, encodeBool(e1), encodeBool(e2))
@@ -65,35 +79,37 @@ class CVC5Backend extends SMTBackend {
     case ImpliesExpr(left, right) => tm.mkTerm(Kind.IMPLIES, encodeBool(left), encodeBool(right))
     // quantifier over state variable
     case Assertion(quantifier, List(AssertVarDecl(vName, StateType())), body) => {
-      val varList = tm.mkTerm(Kind.VARIABLE_LIST, getVarEnv(vName.name, state = true))
+      val newEnvAndVar = env.withState(vName.name)
+      val varList = tm.mkTerm(Kind.VARIABLE_LIST, newEnvAndVar._2)
       val q = quantifier match {
         case "exists" => Kind.EXISTS
         case "forall" => Kind.FORALL
       }
-      tm.mkTerm(q, varList, encodeBool(body))
+      tm.mkTerm(q, varList, encodeBool(body)(newEnvAndVar._1))
     }
     // quantifier over non-state variable
     case Assertion(quantifier, List(AssertVarDecl(vName, _)), body) => {
-      val varList = tm.mkTerm(Kind.VARIABLE_LIST, getVarEnv(vName.name, state = false))
+      val newEnvAndVar = env.withInt(vName.name)
+      val varList = tm.mkTerm(Kind.VARIABLE_LIST, newEnvAndVar._2)
       val q = quantifier match {
         case "exists" => Kind.EXISTS
         case "forall" => Kind.FORALL
       }
-      tm.mkTerm(q, varList, encodeBool(body))
+      tm.mkTerm(q, varList, encodeBool(body)(newEnvAndVar._1))
     }
     case LookupExpr(AssertVar(_), Id(_)) => sys.error("CVC5Backend: Unexpected LookupExpr in boolean conversion: " + expr.toString)
     case LookupExpr(id, index) => encodeBool(ParallelRunner.resolveLookup(index)(id.asInstanceOf[AssertVar]))
-    case StateExistsExpr(AssertVar(name), false) => tm.mkTerm(Kind.APPLY_UF, S, getVarEnv(name, state = true))
-    case StateExistsExpr(AssertVar(name), true) => tm.mkTerm(Kind.APPLY_UF, S_err, getVarEnv(name, state = true))
+    case StateExistsExpr(AssertVar(name), false) => tm.mkTerm(Kind.APPLY_UF, S, env.getState(name))
+    case StateExistsExpr(AssertVar(name), true) => tm.mkTerm(Kind.APPLY_UF, S_err, env.getState(name))
     case _ => sys.error("CVC5Backend: Unexpected expression in boolean conversion: " + expr.toString)
   }
 
-  private def encodeInt(expr: Expr): Term = expr match {
-    case LookupExpr(AssertVar(stateName), Id(varName)) => tm.mkTerm(Kind.SELECT, getVarEnv(stateName, state = true), getConstEnv(varName))
+  private def encodeInt(expr: Expr)(implicit env: VarEnv): Term = expr match {
+    case LookupExpr(AssertVar(stateName), Id(varName)) => tm.mkTerm(Kind.SELECT, env.getState(stateName), getConstEnv(varName))
     case LookupExpr(id, index) => encodeInt(ParallelRunner.resolveLookup(index)(id.asInstanceOf[AssertVar]))
     case Id(name) => getConstEnv(name)
     case Num(value) => tm.mkInteger(value)
-    case AssertVar(name) => getVarEnv(name, state = false)
+    case AssertVar(name) => env.getInt(name)
     case BinaryExpr(e1, op, e2) => {
       val kind = op match {
         case "+" => Kind.ADD
